a4c1ac834a2f2dac419164225175e38cf12d165e
ashch12345 <49492106+ashch12345@users.noreply.github.com>
2019-04-10 23:18:54 +0530

    Add files via upload

---
+++ Untitled3.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from nltk.stem import LancasterStemmer
+stemmerlancaster=LancasterStemmer()
+stemmerlancaster.stem('eating')
+
+
+# In[2]:
+
+
+stemmerlancaster.stem('happy')
+
+
+# In[3]:
+
+
+stemmerlancaster.stem('hating')
+
+
+# In[4]:
+
+
+stemmerlancaster.stem('vote')
+
+
+# In[5]:
+
+
+stemmerlancaster.stem('die')
+
+
+# In[6]:
+
+
+stemmerlancaster.stem('encyclopedia')
+
+
+# In[7]:
+
+
+stemmerlancaster.stem('batterie')
+
+
+# In[8]:
+
+
+stemmerlancaster.stem('pie')
+
+
+# In[9]:
+
+
+stemmerlancaster.stem('devotee')
+
+
+# In[ ]:
+
+
+
+
